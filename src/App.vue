fee5974cfa9b1b9e7e75c8c8e83cf3772bc574f6
JuanDuran85 <duran85juan@gmail.com>
2020-05-22 16:26:51 -0400

    ejemplo copn axios conexion a API

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,45 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>pokemon</h1>
+    <input type="text" v-model="nombre">
+    <button @click="pokebusca">Buscar</button>
+    <hr>
+    <ul>
+      <li v-for="(item, index) in habilidad" :key="index">{{item.ability.name}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      nombre: 'pikachu',
+      habilidad: null
+    }
+  },
+  methods: {
+    pokebusca(){
+      if (this.nombre) {
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${this.nombre}`)
+        .then(response => {
+          console.info(response.data.abilities[0]);
+          this.habilidad = response.data.abilities;
+
+        })
+        .catch(error => {
+          console.log(error)
+          alert("El nombre no existe");  
+        })
+      }else {
+        alert("No hay nombre para buscar");
+      }
+    }
+  },
 }
 </script>
 
