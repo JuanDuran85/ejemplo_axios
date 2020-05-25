7b57d0388344a325ed172e52b529b67ce05152bc
JuanDuran85 <duran85juan@gmail.com>
2020-05-25 14:48:41 -0400

    cambios

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" :src="imagen">
     <h1>pokemon</h1>
     <input type="text" v-model="nombre">
     <button @click="pokebusca">Buscar</button>
     <hr>
-    <div v-for="(item, index) in imagenes" :key="index">
-      <img :src="item" :alt="index">
-    </div>
     <hr>
-    <ul>
-      <li v-for="(item, index) in habilidad" :key="index">{{item.ability.name}}</li>
-    </ul>
+    <div>
+      <p>{{traerNombre}}</p>
+      <div>
+        <img :src="traerImg" alt="img">
+      </div>
+      <ul>
+        <li v-for="(item, index) in habilidad" :key="index">{{item.ability.name}}</li>
+      </ul>
+      <ul>
+        <li v-for="(item, index) in movimiento" :key="index">{{item.move.name}}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -22,9 +27,17 @@ export default {
   name: 'App',
   data() {
     return {
+      datosCompletos: {
+        name: '',
+        sprites: {
+          'front_default': ''
+        }
+      },
+      validar: false,
       nombre: 'pikachu',
       habilidad: null,
       imagenes: null,
+      movimiento: [],
       imagen: require("./assets/logo.png"),
       url: 'https://www.digital55.com/wp-content/uploads/2019/09/%C2%BFQue%CC%81-debe-tener-un-profesional-especialista-en-Vue.png'
     }
@@ -33,10 +46,11 @@ export default {
     pokebusca(){
       if (this.nombre) {
         axios.get(`https://pokeapi.co/api/v2/pokemon/${this.nombre}`)
-        .then(response => {
-          console.info(response.data.sprites);
-          this.imagenes = response.data.sprites;
-          this.habilidad = response.data.abilities;
+        .then(respuesta => {
+          this.datosCompletos = respuesta.data;
+          this.habilidad = respuesta.data.abilities;
+          this.imagenes = respuesta.data.sprites.front_default;
+          this.movimiento = respuesta.data.moves;
         })
         .catch(error => {
           console.log(error)
@@ -45,7 +59,44 @@ export default {
       }else {
         alert("No hay nombre para buscar");
       }
+    },
+    async pikachu(){
+      try {
+        let respuesta = await axios.get('https://pokeapi.co/api/v2/pokemon/pikachu');
+        console.info(respuesta.data);
+        this.datosCompletos = respuesta.data;
+        console.log(this.datosCompletos)
+        this.habilidad = respuesta.data.abilities;
+        this.imagenes = respuesta.data.sprites.front_default;
+        this.movimiento = respuesta.data.moves;
+        this.validar = !this.validar;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    pikachu2(){
+      axios.get('https://pokeapi.co/api/v2/pokemon/pikachu')
+      .then(respuesta => {
+        console.info(respuesta.data);
+        this.datosCompletos = respuesta.data;
+        console.log(this.datosCompletos)
+        this.habilidad = respuesta.data.abilities;
+        this.imagenes = respuesta.data.sprites.front_default;
+        this.movimiento = respuesta.data.moves;
+        this.validar = !this.validar;
+      }).catch (error => console.error(error))
     }
+  },
+  mounted() {
+    this.pikachu2();
+  },
+  computed: {
+    traerNombre(){
+      return this.datosCompletos.name
+    },
+    traerImg(){
+      return this.datosCompletos.sprites.front_default
+    },
   },
 }
 </script>
